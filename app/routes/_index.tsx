93b25f0fc8fd7e0e6b793bf894be0343eaaa60a5
matthew-r-tanudjaja <matthew.r.tanudjaja@linecorp.com>
2023-05-03 21:03:08 +0700

    NO-TICKET Add base code

---
+++ app/routes/_index.tsx
@@ -0,0 +1,80 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+import { Button, Input, Row, Col, Layout } from "antd";
+import { KeyOutlined, UserOutlined } from "@ant-design/icons";
+import axios from "axios";
+import { ChangeEvent, useState } from "react";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "Connect Web" }];
+};
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: 'navy',
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  margin: '10px',
+  padding: '10px',
+  color: '#fff',
+  backgroundColor: 'white',
+};
+
+const rowStyle: React.CSSProperties = {
+  margin: '10px'
+}
+
+export default function Index() {
+  const [employeeId, setEmployeeId] = useState("")
+  const [password, setPassword] = useState("")
+
+  function onEmployeeIdChange(e: ChangeEvent<HTMLInputElement>) {
+    setEmployeeId(e.target.value)
+  }
+  
+  function onPasswordChange(e: ChangeEvent<HTMLInputElement>) {
+    setPassword(e.target.value)
+  }
+
+  function Login() {
+    axios.post(
+      'http://localhost:8080/sessions',
+      {
+        employeeId: employeeId,
+        password: password
+      }
+    ).then(
+      function (response) {
+        console.log(response)
+      }
+    )
+  }
+
+  return (
+    <Layout>
+      <Layout.Header style={headerStyle}> Connect Web </Layout.Header>
+      <Layout.Content style={contentStyle}>
+        <Row>
+          <Col span={8} />
+          <Col span={8}>
+            <Row style={rowStyle}>
+            <Input size="large" placeholder="Employee ID" prefix={<UserOutlined />}
+              onChange={onEmployeeIdChange} value={employeeId}/>
+            <Input.Password size="large" placeholder="Password" prefix={<KeyOutlined />}
+              onChange={onPasswordChange} value={password}/>
+            </Row>
+            <Row style={rowStyle}>
+            <Button size="large" type="primary" onClick={Login} block>Login</Button>
+            </Row>
+          </Col>
+          <Col span={8} />
+        </Row>
+      </Layout.Content>
+    </Layout>
+  );
+}
