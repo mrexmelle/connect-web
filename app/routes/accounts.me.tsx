a13e176702131d5e268f516d8fdcdc0a505aadd4
matthew-r-tanudjaja <matthew.r.tanudjaja@linecorp.com>
2023-05-08 13:11:50 +0700

    NO-TICKET Add switching widget capability

--- app/routes/accounts.me.tsx
+++ app/routes/accounts.me.tsx
@@ -4,15 +4,6 @@ import axios from "axios";
 import { useState } from "react"
 import ProfileWidget from "~/components/ProfileWidget";
 
-const headerStyle: React.CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 64,
-  paddingInline: 50,
-  lineHeight: '64px',
-  backgroundColor: 'navy',
-};
-
 const contentStyle: React.CSSProperties = {
   margin: '10px',
   padding: '10px',
@@ -27,29 +18,42 @@ const siderStyle: React.CSSProperties = {
 
 const siderItems= [
   {
-    key: '1',
+    key: 'MYP',
     icon: <UserOutlined />,
     label: 'My Profile'
   },
   {
-    key: '2',
+    key: 'ORG',
     icon: <ApartmentOutlined />,
     label: 'Organizations'
   }
 ]
 
+function selectWidget(key: string) {
+  switch(key) {
+    case 'MYP':
+      return <ProfileWidget style={contentStyle}/>
+    default:
+      return <p>Unrecognised key</p>
+  }
+}
+
 export default function AccountsMe() {
-  const [siderCollapsed, setSiderCollapsed] = useState<boolean>(false);
+  const [siderCollapsed, setSiderCollapsed] = useState<boolean>(false)
+  const [selectedMenuItem, selectMenuItem] = useState<string>('MYP')
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
       <Layout.Sider collapsible theme="light" style={siderStyle}
         collapsed={siderCollapsed}
         onCollapse={(value) => setSiderCollapsed(value)} >
-        <Menu items={siderItems} mode="inline" defaultSelectedKeys={['1']}/>
+        <Menu items={siderItems} mode="inline"
+          defaultSelectedKeys={['MYP']}
+          selectedKeys={[selectedMenuItem]}
+          onSelect={(e) => selectMenuItem(e.key)}/>
       </Layout.Sider>
       <Layout>
-        <ProfileWidget style={contentStyle}/>
+        {selectWidget(selectedMenuItem)}
         <Layout.Footer style={{ textAlign: 'center' }}>
           Astro Technologies Indonesia ©2023
         </Layout.Footer>
